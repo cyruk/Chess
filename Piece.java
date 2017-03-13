c4ef73ec465cac911ff4794cf3c9f40743518393
Shah <sr1163@rutgers.edu>
2017-03-13 15:23:03 +0000

    Piece.java created online with Bitbucket

---
+++ Piece.java
@@ -0,0 +1,15 @@
+package samp;
+
+/**
+ * Created by Shah on 3/1/2017.
+ */
+public class Piece{
+
+    public String color;
+    public boolean ePos;
+    public String name;
+
+    public String getName(){
+        return name;
+    }
+}
