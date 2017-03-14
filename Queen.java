202bc4536b2b18eeb8707155f6e827500b0275d2
Shah <sr1163@rutgers.edu>
2017-03-14 23:57:28 +0000

    Queen.java created online with Bitbucket

---
+++ Queen.java
@@ -0,0 +1,27 @@
+package samp;
+
+import java.io.IOException;
+
+/**
+ * Created by Shah on 3/9/2017.
+ */
+public class Queen extends Piece {
+
+    public String color;
+    public boolean ePos;
+    public String name;
+    public  boolean moved;
+
+    public Queen(String color, String name){
+        this.color = color;
+        this.name = name;
+        ePos = false;
+        moved = false;
+    }
+    public String isValid(int row1, int col1, int row2, int col22, Board br) throws IOException {return "";}
+
+    public String getName(){
+        return name;
+    }
+    public String getColor(){return color;}
+}
