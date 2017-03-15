164199b8c7fbd52fa90afa6b2548253449d24ac3
Shah <sr1163@rutgers.edu>
2017-03-15 02:37:44 +0000

    Bishop.java edited online with Bitbucket

--- Bishop.java
+++ Bishop.java
@@ -1,4 +1,4 @@
-package samp;
+ samp;
 
 import java.io.IOException;
 
@@ -18,10 +18,86 @@ public class Bishop extends Piece {
         ePos = false;
         moved = false;
     }
-    public String isValid(int row1, int col1, int row2, int col22, Board br) throws IOException {return "";}
+    public Bishop(){}
+    public String isValid(int row1, int col1, int row2, int col2, Board br) throws IOException {
+        String dir = direction(row1,col1,row2,col2);
+        int i,j;
+        String cl = br.board[row1][col1].getColor();
+        if(dir.equals("invalid")){
+            return "No";
+        }
 
+        else if(dir.equals("ne")){
+            for(i=row1-1;i>row2;i--){
+                for(j = col1+1;j<col2;j++){
+                    if(!(br.board[i][j].getClass().isInstance(new Empty()))){
+                        return "No";
+                    }
+                }
+            }
+        }
+        else if(dir.equals("nw")){
+            for(i=row1-1;i>row2;i--){
+                for(j = col1-1;j>col2;j--){
+                    if(!(br.board[i][j].getClass().isInstance(new Empty()))){
+                        return "No";
+                    }
+                }
+            }
+        }
+        else if(dir.equals("se")){
+            for(i=row1+1;i<row2;i++){
+                for(j = col1+1;j<col2;j++){
+                    if(!(br.board[i][j].getClass().isInstance(new Empty()))){
+                        return "No";
+                    }
+                }
+            }
+        }
+        else if(dir.equals("sw")){
+            for(i=row1+1;i<row2;i++){
+                for(j = col1-1;j>col2;j--){
+                    if(!(br.board[i][j].getClass().isInstance(new Empty()))){
+                        return "No";
+                    }
+                }
+            }
+        }
+        if((br.board[row2][col2].getClass().isInstance(new Empty()))) {
+            return "FreeMove";
+        }
+        else if(br.board[row2][col2].getColor().equals(cl)){
+            return "No";
+        }
+        return "Kill";
+
+    }
+
+    public String direction(int row1,int col1, int row2, int col2){
+        double slope = (row2-row1)/(col2-col1);
+        double dX = col2-col1;
+        double dY = row2-row1;
+        if(slope != 1.0 && slope != -1.0){
+            return "invalid";
+        }
+        if(dY>0&& dX<0){
+            return "sw";
+        }
+        else if(dY<0 &&dX<0){
+            return "nw";
+        }
+        else if(dY>0&&dX>0){
+            return "se";
+        }
+        else if(dY<0&&dX>0){
+            return "ne";
+        }
+
+        return "invalid";
+    }
     public String getName(){
         return name;
     }
     public String getColor(){return color;}
 }
+
