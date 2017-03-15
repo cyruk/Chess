a8e86ae22fa524f0c3e2f833147db0d78ecd222f
Shah <sr1163@rutgers.edu>
2017-03-15 03:29:53 +0000

    Queen.java edited online with Bitbucket

--- Queen.java
+++ Queen.java
@@ -18,7 +18,21 @@ public class Queen extends Piece {
         ePos = false;
         moved = false;
     }
-    public String isValid(int row1, int col1, int row2, int col22, Board br) throws IOException {return "";}
+    public Queen(){}
+    public String isValid(int row1, int col1, int row2, int col2, Board br) throws IOException {
+        Rook rook = new Rook();
+        Bishop bishop = new Bishop();
+        if(rook.isValid(row1,col1,row2,col2,br).equals("No") && bishop.isValid(row1,col1,row2,col2,br).equals("No")){
+            return "No";
+        }
+        else if(rook.isValid(row1,col1,row2,col2,br).equals("FreeMove") || bishop.isValid(row1,col1,row2,col2,br).equals("FreeMove")){
+            return "FreeMove";
+        }
+        else if(rook.isValid(row1,col1,row2,col2,br).equals("Kill") || bishop.isValid(row1,col1,row2,col2,br).equals("Kill")){
+            return "Kill";
+        }
+        return "No";
+    }
 
     public String getName(){
         return name;
