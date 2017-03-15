11fac2ec1ddaaf33031f13947a7d90fe9aece3b8
Shah <sr1163@rutgers.edu>
2017-03-15 03:14:03 +0000

    Knight.java edited online with Bitbucket

--- Knight.java
+++ Knight.java
@@ -17,7 +17,51 @@ public class Knight extends Piece {
         ePos = false;
         moved = false;
     }
-    public String isValid(int row1, int col1, int row2, int col22, Board br) throws IOException {return "";}
+    public Knight(){}
+    public String isValid(int row1, int col1, int row2, int col2, Board br) throws IOException {
+        boolean properMove = properMove(row1,col1,row2,col2);
+        String cl = br.board[row1][col1].getColor();
+        if(properMove == false){
+            return "No";
+        }
+        else{
+            if(br.board[row2][col2].getClass().isInstance(new Empty())){
+                return "FreeMove";
+            }
+            else if(br.board[row2][col2].getColor().equals(cl)){
+                return "No";
+            }
+        }
+        return "Kill";
+    }
+
+    public boolean properMove(int row1, int col1, int row2, int col2){
+        if(row2 ==row1-2 && col2 == col1-1){
+            return true;
+        }
+        else if(row2 ==row1-1 && col2 == col1-2){
+            return true;
+        }
+        else if(row2 ==row1-2 && col2 == col1+1){
+            return true;
+        }
+        else if(row2 ==row1-1 && col2 == col1+2){
+            return true;
+        }
+        else if(row2 ==row1+1 && col2 == col1+2){
+            return true;
+        }
+        else if(row2 ==row1+2 && col2 == col1+1){
+            return true;
+        }
+        else if(row2 ==row1+2 && col2 == col1-1){
+            return true;
+        }
+        else if(row2 ==row1+1 && col2 == col1-2){
+            return true;
+        }
+        return false;
+    }
 
     public String getName(){
         return name;
