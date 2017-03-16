4c4dd45117096cf3b841ee75c6811a9e1fbcaa47
Shah <sr1163@rutgers.edu>
2017-03-16 02:01:21 +0000

    Queen.java edited online with Bitbucket

--- Queen.java
+++ Queen.java
@@ -11,12 +11,14 @@ public class Queen extends Piece {
     public boolean ePos;
     public String name;
     public  boolean moved;
+    public int id;
 
-    public Queen(String color, String name){
+    public Queen(String color, String name, int id){
         this.color = color;
         this.name = name;
         ePos = false;
         moved = false;
+        this.id = id;
     }
     public Queen(){}
     public String isValid(int row1, int col1, int row2, int col2, Board br) throws IOException {
@@ -38,4 +40,7 @@ public class Queen extends Piece {
         return name;
     }
     public String getColor(){return color;}
+    public int getId(){
+        return id;
+    }
 }
