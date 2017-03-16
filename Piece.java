e2ad3c45f319db83c555d41ec54ce026917b2f8a
Shah <sr1163@rutgers.edu>
2017-03-16 02:02:05 +0000

    Piece.java edited online with Bitbucket

--- Piece.java
+++ Piece.java
@@ -10,13 +10,18 @@ public class Piece{
     public String color;
     public boolean ePos;
     public String name;
+    public boolean moved;
+    public int id;
 
-    public String isValid(int row1, int col1, int row2, int col22, Board br) throws IOException{return "";}
+    public String isValid(int row1, int col1, int row2, int col2, Board br) throws IOException{return "";}
     public String getName(){
         return name;
     }
     public String getColor(){return color;}
     public void setName(String name){
         this.name = name;
+    }
+    public int getId(){
+        return id;
     }
 }
