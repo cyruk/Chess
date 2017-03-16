a719370c0e39fea5f90239d4f2d8a678403d4836
Shah <sr1163@rutgers.edu>
2017-03-16 02:03:02 +0000

    Board.java edited online with Bitbucket

--- Board.java
+++ Board.java
@@ -5,75 +5,52 @@ package samp;
  */
 public class Board  {
     Piece[][] board = new Piece[9][9];
-
+    String [] Black = new String[16];
+    String [] White = new String[16];
     public Board(){
-        String[] numbers = {"1", "2","3","4","5", "6","7","8"};
-        String[] letters = {"a", "b","c","d","e", "f","g","h"};
-        Pawn wp1 = new Pawn("White","wp");
-        Pawn wp2 = new Pawn("White","wp");
-        Pawn wp3 = new Pawn("White","wp");
-        Pawn wp4 = new Pawn("White","wp");
-        Pawn wp5 = new Pawn("White","wp");
-        Pawn wp6 = new Pawn("White","wp");
-        Pawn wp7 = new Pawn("White","wp");
-        Pawn wp8 = new Pawn("White","wp");
-        Rook wR1 = new Rook("White","wR");
-        Rook wR2 = new Rook("White","wR");
-        King wK = new King("White","wK");
-        Bishop wB1 = new Bishop("White","wB");
-        Bishop wB2 = new Bishop("White","wB");
-        Knight wN1 = new Knight("White","wN");
-        Knight wN2 = new Knight("White","wN");
-        Queen wQ = new Queen("White","wQ");
-        board[6][0] = wp1;
-        board[6][1] = wp2;
-        board[6][2] = wp3;
-        board[6][3] = wp4;
-        board[6][4] = wp5;
-        board[6][5] = wp6;
-        board[6][6] = wp7;
-        board[6][7] = wp8;
-        board[7][0] = wR1;
-        board[7][1] = wN1;
-        board[7][2] = wB1;
-        board[7][3] = wQ;
-        board[7][4] = wK;
-        board[7][5] = wB2;
-        board[7][6] = wN2;
-        board[7][7] = wR2;
+        String[] numbers = {"1","2","3","4","5","6","7","8"};
+        String[] letters = {"a","b","c","d","e","f","g","h"};
+        for(int i = 0;i<8;i++){
+            board[6][i] = new Pawn("White", "wp", i);
+            White[i] = String.valueOf(6) + String.valueOf(i);
+        }
+        board[7][0] = new Rook("White","wR",8);
+        White[8] = String.valueOf(7) + String.valueOf(0);
+        board[7][7] = new Rook("White","wR",9);
+        White[9] = String.valueOf(7) + String.valueOf(7);
+        board[7][1] = new Knight("White","wN",10);
+        White[10] = String.valueOf(7) + String.valueOf(1);
+        board[7][6] = new Knight("White","wN",11);
+        White[11] = String.valueOf(7) + String.valueOf(6);
+        board[7][2] = new Bishop("White","wB",12);
+        White[12] = String.valueOf(7) + String.valueOf(2);
+        board[7][5] = new Bishop("White","wB",13);
+        White[13] = String.valueOf(7) + String.valueOf(5);
+        board[7][3] = new Queen("White","wQ",14);
+        White[14] = String.valueOf(7) + String.valueOf(6);
+        board[7][4] = new King("White","wK", 15);
+        White[15] = String.valueOf(7) + String.valueOf(4);
 
-        Pawn bp1 = new Pawn("Black","bp");
-        Pawn bp2 = new Pawn("Black","bp");
-        Pawn bp3 = new Pawn("Black","bp");
-        Pawn bp4 = new Pawn("Black","bp");
-        Pawn bp5 = new Pawn("Black","bp");
-        Pawn bp6 = new Pawn("Black","bp");
-        Pawn bp7 = new Pawn("Black","bp");
-        Pawn bp8 = new Pawn("Black","bp");
-        Rook bR1 = new Rook("Black","bR");
-        Rook bR2 = new Rook("Black","bR");
-        King bK = new King("Black","bK");
-        Bishop bB1 = new Bishop("Black","bB");
-        Bishop bB2 = new Bishop("Black","bB");
-        Knight bN1 = new Knight("Black","bN");
-        Knight bN2 = new Knight("Black","bN");
-        Queen bQ = new Queen("Black","bQ");
-        board[1][0] = bp1;
-        board[1][1] = bp2;
-        board[1][2] = bp3;
-        board[1][3] = bp4;
-        board[1][4] = bp5;
-        board[1][5] = bp6;
-        board[1][6] = bp7;
-        board[1][7] = bp8;
-        board[0][0] = bR1;
-        board[0][1] = bN1;
-        board[0][2] = bB1;
-        board[0][3] = bQ;
-        board[0][4] = bK;
-        board[0][5] = bB2;
-        board[0][6] = bN2;
-        board[0][7] = bR2;
+        for(int i = 0;i<8;i++){
+            board[1][i] = new Pawn("Black", "bp", i);
+            Black[i] = String.valueOf(1) + String.valueOf(i);
+        }
+        board[0][0] = new Rook("Black","bR",8);
+        Black[8] = String.valueOf(0) + String.valueOf(0);
+        board[0][7] = new Rook("Black","bR",9);
+        Black[9] = String.valueOf(0) + String.valueOf(7);
+        board[0][1] = new Knight("Black","bN",10);
+        Black[10] = String.valueOf(0) + String.valueOf(1);
+        board[0][6] = new Knight("Black","bN",11);
+        Black[11] = String.valueOf(0) + String.valueOf(6);
+        board[0][2] = new Bishop("Black","bB",12);
+        Black[12] = String.valueOf(0) + String.valueOf(2);
+        board[0][5] = new Bishop("Black","bB",13);
+        Black[13] = String.valueOf(0) + String.valueOf(5);
+        board[0][3] = new Queen("Black","bQ",14);
+        Black[14] = String.valueOf(0) + String.valueOf(3);
+        board[0][4] = new King("Black","bK",15);
+        Black[15] = String.valueOf(0) + String.valueOf(4);
 
         for(int i = 2;i <6;i++){
             for(int j = 0;j<8;j++){
@@ -110,7 +87,7 @@ public class Board  {
                     System.out.printf("%-3s", board[i][j].getName());
                 }
                 else if(i == 8){
-                    System.out.printf("%-3s",board[i][j].getName());
+                    System.out.printf("%-3.9s",board[i][j].getName());
                 }
                 else
                     System.out.print(board[i][j].getName() + " ");
