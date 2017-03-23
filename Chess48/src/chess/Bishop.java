bd943fd3bf42e19f7f146747aa8c9e2a2cf3c35a
Shah <sr1163@rutgers.edu>
2017-03-23 00:44:46 +0000

    Bishop.java edited online with Bitbucket

--- Chess48/src/chess/Bishop.java
+++ Chess48/src/chess/Bishop.java
@@ -3,21 +3,44 @@ package chess;
 import java.io.IOException;
 
 /**
- * Created by Shah on 3/9/2017.
+ * The Bishop class is inheriting piece and added to the polymorphic 2d array of type Piece
+ * @author Shah Rahim, John Chen
+ *
  */
 public class Bishop extends Piece {
 
     public String color;
     public String name;
     public int id;
-
+    
+    /**
+     * The bishop constructor
+     * @param color sets the color of the bishop piece
+     * @param name sets the name of the bishop piece
+     * @param id sets the id number for the bishop piece
+     */
     public Bishop(String color, String name, int id){
         this.color = color;
         this.name = name;
         this.id = id;
 
     }
+    
+    /**
+     * No arg constructor
+     */
     public Bishop(){}
+    
+    /**
+     * This method is used to check if a move is valid for the Bishop
+     * @param row1 this is the starting row of a piece
+     * @param col1 this is the starting column of a piece
+     * @param row2 this is the desired row a piece wants to move
+     * @param col2 this is the desired column a piece wants to move
+     * @param br this is the board being passed in for checking
+     * @return returns a string based on the pieces possible moves: FreeMove, Kill or their special respective move or no for invalid
+     * @throws IOException
+     */
     public String isValid(int row1, int col1, int row2, int col2, Board br) throws IOException {
         String dir = direction(row1,col1,row2,col2);
         int i,j;
@@ -75,7 +98,15 @@ public class Bishop extends Piece {
         return "Kill";
 
     }
-
+    
+    /**
+     * This method calculates what direction the bishop is going
+     * @param row1 this is the starting row of a piece
+     * @param col1 this is the starting column of a piece
+     * @param row2 this is the desired row a piece wants to move
+     * @param col2 this is the desired column a piece wants to move
+     * @return returns a string either; invalid, or the the direction the bishop is going
+     */
     public String direction(int row1,int col1, int row2, int col2){
         double dX = col2-col1;
         double dY = row2-row1;
@@ -97,10 +128,24 @@ public class Bishop extends Piece {
         }
         return "invalid";
     }
+    /**
+     * This method is used to get the color of a piece
+     * @return returns a string for the color of a certain piece
+     */
+    public String getColor(){
+        return color;
+    }
+    /**
+     * Used to get the name of the piece
+     * @return returns a string for the name of the given piece
+     */
     public String getName(){
         return name;
     }
-    public String getColor(){return color;}
+    /**
+     * This is used to get the id number of the rook piece
+     * @return an int for the rook id
+     */
     public int getId(){
         return id;
     }
